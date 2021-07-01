0e238205597722e8c6a02826f0c88bb89fb2db70
B3rl1 <kirill.skv.2000@mail.ru>
2021-07-01 20:34:27 +0300

    Обновил боевую систему, инвентарь, счётчик
    Обновил стрельбу у персонажа
    Добавил подбор предметов
    Добавил счётчик для очков

--- Assets/Scripts/Player/Character Stats/CharacterStats.cs
+++ Assets/Scripts/Player/Character Stats/CharacterStats.cs
@@ -3,11 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//Перечисление для характеристик персонажа
+public enum CharacterStatsType
+{
+	HelthPoint,
+	AttackRange,
+	AttackSpeed,
+	Damage,
+	Speed
+}
+
 public class CharacterStats : MonoBehaviour
 {
+	public float Speed = 5f;
 	public float HealthPoint = 100f;
 	public float ArmorPoint = 2f;
-
 	public float Damage = 10f;
 	public float AttackSpeed = 1.2f;
 	private float _attackReload;
@@ -24,6 +35,32 @@ public class CharacterStats : MonoBehaviour
 			_attackReload -= Time.deltaTime;
 	}
 
+	public void ModifyValue(CharacterStatsType statType, float value)
+	{
+		switch (statType)
+		{
+			case CharacterStatsType.HelthPoint:
+				HealthPoint += value;
+				Debug.Log("Поднял здоровье = " + HealthPoint);
+				break;
+			case CharacterStatsType.Damage:
+				Damage += value;
+				Debug.Log("Поднял урон = " + Damage);
+				break;
+			case CharacterStatsType.AttackSpeed:
+				AttackSpeed += value;
+				Debug.Log("Поднял скорость атаки = " + AttackSpeed);
+				break;
+			case CharacterStatsType.AttackRange:
+				AttackRange += value;
+				Debug.Log("Поднял дальность атаки = " + AttackRange);
+				break;
+			case CharacterStatsType.Speed:
+				Speed += value;
+				Debug.Log("Поднял скорость передвижения = " + Speed);
+				break;
+		}
+	}
 
 	public void GetDamage(float damage)
 	{
@@ -38,14 +75,9 @@ public class CharacterStats : MonoBehaviour
 		}
 	}
 
-	private void OnDrawGizmos()
-	{
-		Gizmos.color = Color.red;
-		Gizmos.DrawSphere(transform.position, AttackRange);
-	}
-
 	public void DealDamage(float damage, IDamage target)
 	{
 		target.GetDamage(damage);
 	}
 }
+
--- Assets/Scripts/Player/InputHandler.cs
+++ Assets/Scripts/Player/InputHandler.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class InputHandler : MonoBehaviour
 {
     public Vector2 InputVector { get; private set; }
-
     public Vector3 MousePosition { get; private set; }
 
     void Update()
